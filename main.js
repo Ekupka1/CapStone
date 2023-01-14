a3dbbe5a88a6eb045dcb9dc908acbe87be1a8b76
Ethan K <ekupka3@gmail.com>
2023-01-13 17:56:28 -0700

    Fixed a bug with the sug and email then worked on the first blog post

--- main.js
+++ main.js
@@ -40,51 +40,50 @@ const msg = document.getElementById('msg')
             })
             .catch(error => console.error('Error!', error.message))
           })
-         
+
 /* Home Typing Text ------------------------------------------------------------------- */
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+// const typedTextSpan = document.querySelector(".typed-text");
+// const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Welcome!"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
+// const textArray = ["Welcome!"];
+// const typingDelay = 200;
+// const erasingDelay = 100;
+// const newTextDelay = 2000; // Delay between current and next text
+// let textArrayIndex = 0;
+// let charIndex = 0;
 
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
+// function type() {
+//   if (charIndex < textArray[textArrayIndex].length) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+//     charIndex++;
+//     setTimeout(type, typingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     setTimeout(erase, newTextDelay);
+//   }
+// }
 
-function erase() {
-  if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
+// function erase() {
+//   if (charIndex > 0) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+//     charIndex--;
+//     setTimeout(erase, erasingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     textArrayIndex++;
+//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
+//     setTimeout(type, typingDelay + 1100);
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
-
+// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+//   if(textArray.length) setTimeout(type, newTextDelay + 250);
+// });
+         
 /* Blog Type Sort ------------------------------------------------------------------- */
 function myFunction() {
   var input, filter, cards, cardContainer, h5, title, i;
